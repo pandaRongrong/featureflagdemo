967f15d0fdd11107a6a7e038c1844319f135d389
pandarongrong <18200184962@163.com>
2025-02-27 23:22:49 +0800

    provider能够读取本地文件

--- flagczr/src/main/java/com/czr/controller/OpenFeatureBeans.java
+++ flagczr/src/main/java/com/czr/controller/OpenFeatureBeans.java
@@ -1,6 +1,11 @@
 package com.czr.controller;
 
+import com.networknt.schema.FalseValidator;
+import dev.openfeature.contrib.providers.flagd.Config;
+import dev.openfeature.contrib.providers.flagd.FlagdOptions;
+import dev.openfeature.contrib.providers.flagd.FlagdProvider;
 import dev.openfeature.sdk.OpenFeatureAPI;
+import dev.openfeature.sdk.exceptions.OpenFeatureError;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +15,19 @@ public class OpenFeatureBeans {
     public OpenFeatureAPI openFeatureAPI() {
         final OpenFeatureAPI openFeatureAPI = OpenFeatureAPI.getInstance();
 
+        // Use flagd as the OpenFeature provider and use default configurations
+        try {
+            FlagdProvider flagdProvider = new FlagdProvider(
+                    FlagdOptions.builder()
+                            .resolverType(Config.Resolver.FILE)
+                            .offlineFlagSourcePath("flagczr/src/main/resources/flags.json")
+                            .build());
+            openFeatureAPI.setProviderAndWait(flagdProvider);
+
+
+        } catch (OpenFeatureError e) {
+            throw new RuntimeException("Failed to set OpenFeature provider", e);
+        }
         return openFeatureAPI;
     }
 }
--- flagczr/src/main/java/com/czr/controller/RestHello.java
+++ flagczr/src/main/java/com/czr/controller/RestHello.java
@@ -4,7 +4,9 @@ import dev.openfeature.sdk.Client;
 import dev.openfeature.sdk.OpenFeatureAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class RestHello {
     private final OpenFeatureAPI openFeatureAPI;
 
@@ -13,7 +15,7 @@ public class RestHello {
         this.openFeatureAPI = OFApi;
     }
 
-    @GetMapping("/hello")
+    @GetMapping("/hello2")
     public String getHello() {
         final Client client = openFeatureAPI.getClient();
 
